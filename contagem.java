fe9575e6eadd15ce36ba73d23878ff33e8b16caf
RaphaelHZ <83135772+RaphaelHZ@users.noreply.github.com>
2021-08-18 00:47:01 -0300

    exercicio de contagem de caracteres

---
+++ contagem.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class contagem {
+    static String texto = "";
+    static int valor = 0;
+    String caractere ="";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite alguns caracteres, podem ser letras ou números: ");
+        texto = scanner.nextLine();
+        scanner.close();
+        int letras = contarLetras(texto, 0);
+        valor = 0;
+        int numeros = contarNumeros(texto, 0);
+        valor = 0;
+        int cEspeci = contarCaracteresEspeciais(texto, 0);
+        int resultado = letras + numeros + cEspeci;
+        System.out.println("No texto existem " + letras + " letras e " + numeros + " números e " + cEspeci + " caracteres especiais.");
+        System.out.println("Total de caracteres: " + resultado);
+    }
+
+    public static int contarLetras(String texto, int caracter){
+        if (texto.equals("")) return 0;
+        if ((caracter + 1) > texto.length()) return valor;
+        char letra = texto.charAt(caracter);
+        if (((letra >= 65) && (letra <= 90)) || ((letra >= 97) && (letra <= 122)) ){
+            valor = valor + 1;
+            contarLetras(texto, caracter + 1);
+        } else contarLetras(texto, caracter + 1);
+        return valor;
+    }
+
+    public static int contarNumeros(String texto, int caracter){
+        if (texto.equals("")) return 0;
+        if ((caracter + 1) > texto.length()) return valor;
+        char letra = texto.charAt(caracter);
+        if ((letra > 47) && (letra < 58)){
+            valor = valor + 1;
+            contarNumeros(texto, caracter + 1);
+        } else contarNumeros(texto, caracter + 1);
+        return valor;
+    }
+
+    public static int contarCaracteresEspeciais(String texto, int caracter){
+        if (texto.equals("")) return 0;
+        if ((caracter + 1) > texto.length()) return valor;
+        char letra = texto.charAt(caracter);
+        if ((letra < 48) || ((letra > 57) && (letra < 65)) || ((letra > 90) && (letra < 97)) || (letra > 122)){
+            valor = valor + 1;
+            contarCaracteresEspeciais(texto, caracter + 1);
+        } else contarCaracteresEspeciais(texto, caracter + 1);
+        return valor;
+    }
+}
+
+/*
+Entidade: Unifil EAD
+Nome: Raphael Henrique Zanluqui
+Data: 18-08-2021 00:45
+Objetivo: Contar caracteres utilizando recursão em java
+*/
